f4149c002241bb844c338866e25349913041111d
hamza-melki <93046824+hamza-melki@users.noreply.github.com>
2022-07-16 12:57:21 +0200

    intial

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+
+
+
+
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './boutique/sidebar/sidebar.component';
+import { NavbarComponent } from './boutique/navbar/navbar.component';
+import { ContentComponent } from './boutique/content/content.component';
+import { BoutiqueComponent } from './boutique/boutique.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    NavbarComponent,
+    ContentComponent,
+    BoutiqueComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
